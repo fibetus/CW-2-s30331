a080132531bf1fe490bdc18e39573fb13109d40b
fibetus <s30331@pjwstk.edu.pl>
2025-03-22 20:18:42 +0100

    Add ContainerC

---
+++ ContainerManager/ContainerC.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ContainerManager;
+
+public class ContainerC : Container
+{
+    public double Temperature { get; }
+    public string ProductType { get; }
+    
+    public ContainerC(string productType, double temperature, double height, double conatinerWeight, double depth, double maxLoadWeight) 
+        : base("C", height, conatinerWeight, depth, maxLoadWeight)
+    {
+        Temperature = temperature;
+        ProductType = productType;
+    }
+    
+}
